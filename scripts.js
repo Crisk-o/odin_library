36fa56fca227ad243fb1278dbff39891bcad6108
Jacob Childs <crisko64@gmail.com>
2026-02-10 16:09:28 -0600

    Configured of topbar and library container. Need to add fadeIn animation to new books as they are added in.

--- scripts.js
+++ scripts.js
@@ -26,6 +26,8 @@ function addBookToLibrary(){
     const bookRead = document.getElementById('book-alreadyRead');
     const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
     myLibrary.push(book); /* Use .value of property to get the actual value from form input*/
+    printLibrary();
+    formDialog.close();
 };
 /*Grabs form and prevents data submission*/
 const form = document.getElementById("book-form");
@@ -34,47 +36,62 @@ form.addEventListener('submit', addBookToLibrary);
 /* prints lib w/ button */
 const printLibBtn = document.getElementById("print-library");
 const libraryContainer = document.getElementById("library-container");
+const newBookCardContainer = document.getElementById("newBookCardContainer");
+
 function printLibrary(){
-    libraryContainer.innerHTML = "";
-    libraryContainer.classList.add("unhidden");
-    // libraryContainer.classList.toggle("is-visible");
+    newBookCardContainer.innerHTML = "";
     for(let i = 0; i < myLibrary.length; i++){
         const bookCardDiv = document.createElement('div');
         bookCardDiv.classList.add("bookCard");
+
         const bookCardInnerDiv = document.createElement('div');
         bookCardInnerDiv.classList.add("bookCardInnerDiv");
-        // generates h2 element with book title
-        const bookCardTitle = document.createElement('h2');
-        bookCardTitle.textContent = "Title: " + myLibrary[i].title;
-        bookCardInnerDiv.appendChild(bookCardTitle);
 
-        // generates h3 element with book author
-        const bookCardAuthor = document.createElement('h3');
-        bookCardAuthor.textContent = "Author: " + myLibrary[i].author;
-        bookCardInnerDiv.appendChild(bookCardAuthor);
-
-        // generates h3 element stating number of pages in book
-        const bookCardPages = document.createElement('h3');
-        bookCardPages.textContent = "No. Pages: " + myLibrary[i].noPages;
-        bookCardInnerDiv.appendChild(bookCardPages);
-
-        // generates h3 element stating if book has been read or not
-        const bookCardRead = document.createElement('h3');
-        bookCardRead.textContent = "Already Read?: " + myLibrary[i].alreadyRead.toUpperCase();
-        bookCardInnerDiv.appendChild(bookCardRead);
+        const bookDetailsList = document.createElement('ul');
+        const bookDetailsTitle = document.createElement('li');
+        bookDetailsTitle.textContent = "Title: " + myLibrary[i].title;
+        const bookDetailsAuthor = document.createElement('li');
+        bookDetailsAuthor.textContent = "Author: " + myLibrary[i].author;
+        const bookDetailsPages = document.createElement('li');
+        bookDetailsPages.textContent = "Pages: " + myLibrary[i].noPages;
+        const bookDetailsRead = document.createElement('li');
+        // converts alreadyRead value to "Yes" or "No" for better readability
+        if(myLibrary[i].alreadyRead.toLowerCase() === 'on' || myLibrary[i].alreadyRead.toLowerCase() === 'true' || myLibrary[i].alreadyRead.toLowerCase() === 'yes'){
+            myLibrary[i].alreadyRead = "Yes";
+        }
+        else{
+            myLibrary[i].alreadyRead = "No";
+        }
+        bookDetailsRead.textContent = "Read: " + myLibrary[i].alreadyRead;
+        bookDetailsList.append(bookDetailsTitle, bookDetailsAuthor, bookDetailsPages, bookDetailsRead);
+        bookCardInnerDiv.appendChild(bookDetailsList);
+    
         // remove book button
         const bookRemoveBtn = document.createElement('button');
+        bookRemoveBtn.classList.add("bookCardBtns");
         bookRemoveBtn.textContent = "Remove Book";
-
         // add event listener to remove book from library array and reprint library
         bookRemoveBtn.addEventListener('click', () => {
             myLibrary.splice(i, 1);
             printLibrary();
         }); 
-        
+        // button to toggle alreadyRead value
+        const bookToggleReadBtn = document.createElement('button');
+        bookToggleReadBtn.classList.add("bookCardBtns");
+        bookToggleReadBtn.textContent = "Mark as Read";
+        bookToggleReadBtn.addEventListener('click', () => {
+            if(myLibrary[i].alreadyRead === "No"){
+                myLibrary[i].alreadyRead = "Yes";
+            }
+            else{       
+                myLibrary[i].alreadyRead = "No";
+            }
+            printLibrary();
+        });
+        bookCardInnerDiv.append(bookRemoveBtn, bookToggleReadBtn);
         bookCardDiv.appendChild(bookCardInnerDiv);
-        bookCardDiv.appendChild(bookRemoveBtn);
-        libraryContainer.appendChild(bookCardDiv);
+        newBookCardContainer.appendChild(bookCardDiv);
+        libraryContainer.appendChild(newBookCardContainer);
         document.body.append(libraryContainer);
     }
 };
